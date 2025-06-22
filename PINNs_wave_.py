e24fe2f23c4cdc6ebcf58299cb78912a8b0b6eac
SARAHP\sarab <s.betancur.giraldo@student.tue.nl>
2025-06-22 11:22:45 +0200

    add solution

--- PINNs_wave_.py
+++ PINNs_wave_.py
@@ -1,6 +1,8 @@
 #%% Import libraries
 import numpy as np
 import matplotlib.pyplot as plt
+import matplotlib.animation as animation
+from IPython.display import HTML
 
 #%% Function definition
 def u_0(x):
@@ -17,17 +19,57 @@ def u_0(x):
         return 0
 
 #%% Define parameters and grid
-dx = 1e-2
-grid_x = np.linspace(0, 1, int(1/dx) + 1)
-u_0_values = np.array([u_0(x) for x in grid_x])
+
+x_lower, x_upper = 0, 1
+t_0, n_t = 0, 800
+c = 1
+dx = 0.01
+dt = 0.1 * dx / c
+x_points = np.linspace(x_lower, x_upper, int((x_upper - x_lower) / dx) + 1)
+t_points = np.linspace(0, n_t * dt, n_t + 1)
+
+# Evaluate initial condition at x points
+u_0_values = np.array([u_0(x) for x in x_points])
 
 #%% Plot initial condition
 plt.figure(figsize=(8, 4))
-plt.plot(grid_x, u_0_values, label='Initial Condition $u_0(x)$', color='blue')
+plt.plot(x_points, u_0_values, label='Initial Condition $u_0(x)$', color='blue')
 plt.title('Dirac Initial Condition')
 plt.xlabel('x')
 plt.ylabel('$u_0(x)$')
 
+#%% 
+
+def f_periodic_extension(x):
+    if (x // 1) % 2 == 1:
+        return -u_0(x % 1)
+    else:
+        return u_0(x % 1)
+
+
+def u_dalembert(x, t):
+    return (f_periodic_extension(x - t) + f_periodic_extension(x + t)) / 2
+
+
+u_exact = np.zeros((len(x_points), len(t_points)))
+for x_index, x_point in enumerate(x_points):
+    for t_index, t_point in enumerate(t_points):
+        u_exact[x_index, t_index] = u_dalembert(x_point, t_point)
+
+#%%
+
+fig, ax = plt.subplots()
+line, = ax.plot(x_points, u_exact[:, 0])
+def animate_exact(i):
+    line.set_data((x_points, u_exact[:, i]))
+    time_step = np.round(t_points[i], 3)
+    plt.title('Time: ' + str(time_step))
+    plt.xlabel('$x$')
+    plt.ylabel('$u(x, ' + str(time_step) + ')$')
+    return (line,)
+
+anim = animation.FuncAnimation(fig, animate_exact, frames=np.arange(0, n_t, 40), interval=100, blit=True)
+HTML(anim.to_jshtml())
 
 
 #%%
